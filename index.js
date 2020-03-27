0ea3d706140214d7af51cdeca3586ae57a68de6d
Thanee Charattrakool <60010465@kmitl.ac.th>
2020-03-27 14:20:19 +0700

    refactor code

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const auth = require('./auth.json');
 const config = require('./config.json');
-const ytdl = require('ytdl-core');
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -12,23 +10,8 @@ client.on('message', async (msg) => {
     const message = msg.content;
 
     if (message.includes("ควย")){
-        msg.channel.send('ไหนใครมีเหี้ยไร');
-        msg.reply('มึงเหรอ');
-    }
-    if (message.startsWith(`${config.prefix}play`)){
-        const connection = await msg.member.voice.channel.join();
-        msg.channel.send('เดี๋ยวผมเล่นให้เลยครับพี่');
-        setTimeout(()=>{
-            connection.play(ytdl(message.slice(config.prefix.length+5), { filter: 'audioonly' }));
-        }, 200)
-        
-    }
-    else if (msg.member.voice.channel && !msg.author.bot) {
-        const connection = await msg.member.voice.channel.join();
-        setTimeout(()=>{
-            connection.play('./sound/pen-kuy-rai.mp3')
-        }, 200)
+        msg.channel.send('หน้ามึงสิ๊');
     }
 });
 
-client.login(process.env.BOT_TOKEN || auth.token);
+client.login(process.env.BOT_TOKEN || config.token);
