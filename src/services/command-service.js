8cacb886a74e52aba68908e1e69c7034a2949f4d
Thanee Charattrakool <60010465@kmitl.ac.th>
2020-03-27 15:38:31 +0700

    add playYoutube and stop command

--- src/services/command-service.js
+++ src/services/command-service.js
@@ -1,3 +1,31 @@
-module.exports = (msg,prefix) => {
-    
+const ytdl = require('ytdl-core');
+
+const stopBot = async (msg) => {
+    if (msg.member.voice.channel) {
+        const connection = await msg.member.voice.channel.join();
+        setTimeout(() => {
+            connection.play('./src/sound/ok.mp3')
+        }, 200)
+    }
+}
+
+const playYoutube = async (msg, prefix) => {
+    if (msg.member.voice.channel) {
+        const connection = await msg.member.voice.channel.join();
+        msg.channel.send('เดี๋ยวผมเล่นให้เลยครับพี่');
+        setTimeout(() => {
+            connection.play(ytdl(msg.content.slice(prefix.length + 5), {
+                filter: 'audioonly'
+            }));
+        }, 200)
+    }
+}
+
+module.exports = (msg, prefix) => {
+    if (msg.content == `${prefix}stop`) {
+        stopBot(msg);
+    } else if (msg.content.startsWith(`${prefix}play`)) {
+        playYoutube(msg, prefix);
+    }
+
 }
