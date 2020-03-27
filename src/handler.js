87bda7a9602f0f2333942d91a1122333d59229a3
Thanee Charattrakool <60010465@kmitl.ac.th>
2020-03-27 15:39:31 +0700

    add command handle

--- src/handler.js
+++ src/handler.js
@@ -4,7 +4,7 @@ const commandService = require('./services/command-service');
 const config = require('../config.json');
 
 module.exports = (msg) => {
-    if (msg.content.startsWith(config.prefix)) {
+    if (msg.content.startsWith(config.prefix) && !msg.author.bot) {
         commandService(msg,config.prefix);
     } else {
         chatService(msg);
