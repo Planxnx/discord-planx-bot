fe886d470bfeb8d126aa42ee12c70888526075f2
Thanee Charattrakool <60010465@kmitl.ac.th>
2020-03-27 14:48:09 +0700

    refactor code

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
+const handler = require('./src/handler');
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', async (msg) => {
-    const message = msg.content;
-
-    if (message.includes("ควย")){
-        msg.channel.send('หน้ามึงสิ๊');
-    }
-});
+client.on('message', msg => handler(msg));
 
 client.login(process.env.BOT_TOKEN || config.token);
---
+++ src/services/chat-service.js
@@ -0,0 +1,20 @@
+const playPenKuyRai = (msg) => {
+    if (msg.member.voice.channel && !msg.author.bot) {
+        msg.member.voice.channel.join()
+            .then(connection => {
+                setTimeout(()=>{
+                    connection.play('./sound/pen-kuy-rai.mp3')
+                }, 200)
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+}
+
+module.exports = (msg) => {
+    if (message.includes("ควย")){
+        msg.channel.send('ไหนใครมีเหี้ยไร');
+        msg.reply('มึงเหรอ');
+    }
+}
