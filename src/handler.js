34ff5550d9c74ca8d0214c276ec6d1a6a36b94f0
Thanee Charattrakool <60010465@kmitl.ac.th>
2020-03-28 09:23:28 +0700

    get BOT_PREFIX env var

--- src/handler.js
+++ src/handler.js
@@ -5,7 +5,7 @@ const config = require('../config.json');
 
 module.exports = (msg) => {
     if (msg.content.startsWith(config.prefix) && !msg.author.bot) {
-        commandService(msg, process.env.BOT_TOKEN || config.prefix);
+        commandService(msg, process.env.BOT_PREFIX || config.prefix);
     } else {
         chatService(msg);
     }
